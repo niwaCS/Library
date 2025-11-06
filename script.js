9ca6a3c25be4d6e6edb3c0185bde5f653823c56b
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2025-11-06 15:47:57 -0500

    Add book constructor

---
+++ script.js
@@ -0,0 +1,18 @@
+const myLibrary = [];
+
+function Book(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.id = crypto.randomUUID();
+    this.read = read;
+
+
+}
+
+function addBookToLibrary(book, id) {
+
+
+
+
+}
